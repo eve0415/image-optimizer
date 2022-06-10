e1f5c7882d559af262ebeb0238759e09c1dc115c
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-06-10 23:21:01 +0900

    feat(etag): support etag

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 import { Type } from '@sinclair/typebox';
-import axios, { AxiosAdapter, AxiosError } from 'axios';
+import axios, { AxiosAdapter } from 'axios';
 import {
     cacheAdapterEnhancer,
     throttleAdapterEnhancer,
@@ -40,7 +40,11 @@ server.get(
                 url: Type.String({ format: 'uri' }),
                 width: Type.Optional(Type.Number()),
                 height: Type.Optional(Type.Number()),
-                quality: Type.Number({ minimum: 1, maximum: 100, default: 80 }),
+                quality: Type.Number({
+                    minimum: 1,
+                    maximum: 100,
+                    default: 80,
+                }),
                 blur: Type.Boolean({ default: false }),
                 format: Type.Optional(
                     Type.String({
@@ -66,71 +70,79 @@ server.get(
             return Promise.reject(new Error('The provided url is not allowed'));
         }
 
-        const baseImage = await imageRequest
-            .get<ArrayBuffer>(request.query.url, {
-                responseType: 'arraybuffer',
-            })
-            .catch((e: AxiosError) => {
-                reply.code(500);
-                return Promise.reject(new Error(e.message));
+        try {
+            const baseImage = await imageRequest.get<ArrayBuffer>(
+                request.query.url,
+                {
+                    responseType: 'arraybuffer',
+                }
+            );
+            const baseFormat = baseImage.headers['content-type']?.startsWith(
+                'image/'
+            )
+                ? baseImage.headers['content-type']?.replace('image/', '')
+                : request.query.url.split('.').pop();
+            const format = request.raw.headers['user-agent']?.includes(
+                '+https://discordapp.com'
+            )
+                ? 'webp'
+                : request.query.format ??
+                  (
+                      request
+                          .accepts()
+                          .type([
+                              'image/avif',
+                              'image/webp',
+                              'image/tiff',
+                              'image/gif',
+                              'image/png',
+                              'image/jpeg',
+                          ]) as string
+                  ).replace('image/', '') ??
+                  baseFormat;
+            const optimizer = sharp(Buffer.from(baseImage.data), {
+                sequentialRead: true,
             });
-        const baseFormat = baseImage.headers['content-type']?.startsWith(
-            'image/'
-        )
-            ? baseImage.headers['content-type']?.replace('image/', '')
-            : request.query.url.split('.').pop();
-        const format = request.raw.headers['user-agent']?.includes(
-            '+https://discordapp.com'
-        )
-            ? 'webp'
-            : request.query.format ??
-              (
-                  request
-                      .accepts()
-                      .type([
-                          'image/avif',
-                          'image/webp',
-                          'image/tiff',
-                          'image/gif',
-                          'image/png',
-                          'image/jpeg',
-                      ]) as string
-              ).replace('image/', '') ??
-              baseFormat;
-        const optimizer = sharp(Buffer.from(baseImage.data), {
-            sequentialRead: true,
-        });
 
-        if (request.query.width || request.query.height) {
-            const metadata = await optimizer.metadata();
-            const width =
-                request.query.width && metadata.width
-                    ? metadata.width >= request.query.width
-                        ? request.query.width
-                        : null
-                    : null;
-            const height =
-                request.query.height && metadata.height
-                    ? metadata.height >= request.query.height
-                        ? request.query.height
-                        : null
-                    : null;
+            if (request.query.width || request.query.height) {
+                const metadata = await optimizer.metadata();
+                const width =
+                    request.query.width && metadata.width
+                        ? metadata.width >= request.query.width
+                            ? request.query.width
+                            : null
+                        : null;
+                const height =
+                    request.query.height && metadata.height
+                        ? metadata.height >= request.query.height
+                            ? request.query.height
+                            : null
+                        : null;
 
-            optimizer.resize(width, height, { fit: 'outside' });
+                optimizer.resize(width, height, { fit: 'outside' });
+            }
+
+            if (request.query.blur) optimizer.blur(5);
+
+            reply.header(
+                'Cache-Control',
+                'public, max-age=14400, s-maxage=84000'
+            );
+            reply.header('Cloudflare-CDN-Cache-Control', 'max-age=24400');
+            reply.header('CDN-Cache-Control', 'max-age=18000');
+            reply.header('Content-Type', `image/${format}`);
+
+            return optimizer
+                .toFormat(format as keyof FormatEnum, {
+                    quality: request.query.quality,
+                })
+                .toBuffer();
+        } catch (e) {
+            reply.code(500);
+            return Promise.reject(
+                e instanceof Error ? new Error(e.message) : e
+            );
         }
-
-        if (request.query.blur) optimizer.blur(5);
-
-        reply.header('Cache-Control', 'public, max-age=14400, s-maxage=84000');
-        reply.header('Cloudflare-CDN-Cache-Control', 'max-age=24400');
-        reply.header('CDN-Cache-Control', 'max-age=18000');
-        reply.header('Content-Type', `image/${format}`);
-
-        return optimizer
-            .toFormat(format as keyof FormatEnum, {
-                quality: request.query.quality,
-            })
-            .toBuffer();
     }
 );
 
@@ -141,6 +153,7 @@ server.get(
 (async () => {
     await server.register(import('@fastify/cors'));
     await server.register(import('@fastify/accepts'));
+    await server.register(import('@fastify/etag'));
 
     await server
         .listen({ port: 8080, host: '0.0.0.0' })
